95ef155a718574d26971feca9ba86b02f6dc684f
Juan David Hermoso <juandavidhermoso@gmail.com>
2017-01-08 13:59:18 +0100

    Menu started

---
+++ application/views/header/desktop_nav.php
@@ -0,0 +1,6 @@
+<ul class="right hide-on-med-and-down">
+  <li><a href="sass.html">Sass</a></li>
+  <li><a href="badges.html">Components</a></li>
+  <li><a href="collapsible.html">Javascript</a></li>
+  <li><a href="mobile.html">Mobile</a></li>
+</ul>
--- application/views/home.php
+++ application/views/home.php
@@ -12,11 +12,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <link rel="stylesheet" href="<?php echo VENDORSPATH; ?>materialize/dist/css/materialize.css">
 </head>
 <body>
+  <header>
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Logo</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <?php require('header/desktop_nav.php'); ?>
+        <?php require('header/mobile_nav.php'); ?>
+      </div>
+    </nav>
+
+  </header>
   <script src="<?php echo VENDORSPATH; ?>jquery/dist/jquery.min.js"></script>
   <script src="<?php echo VENDORSPATH; ?>underscore/underscore-min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js"></script>
   <script src="<?php echo VENDORSPATH; ?>materialize/dist/js/materialize.js"></script>
   <script src="<?php echo JSPATH; ?>router/router.js"></script>
   <script src="<?php echo JSPATH; ?>app.js"></script>
+  <script>
+    $(function() {
+      $(".button-collapse").sideNav();
+    });
+  </script>
 </body>
 </html>
