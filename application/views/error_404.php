ca68613e705367a7fb71d7fb6ff4f7afbddba346
Juan David Hermoso <juandavidhermoso@gmail.com>
2017-02-10 22:51:39 +0100

    Prepared for building Codeigniter404 Page

---
+++ application/views/error_404.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8"/>
+  <title>Backbone.js Library</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo CSSPATH; ?>main.css">
+  <link rel="stylesheet" href="<?php echo VENDORSPATH; ?>materialize/dist/css/materialize.css">
+</head>
+<body>
+
+</body>
+</html>
