36aad0337f6e4b928b105869fe1fd85f64746077
Juan David Hermoso <juandavidhermoso@gmail.com>
2017-03-01 08:52:53 +0100

    Fixed router for catching almost everything

--- app/scripts/router/main.router.js
+++ app/scripts/router/main.router.js
@@ -2,21 +2,31 @@ var app = app || {};
 
 app.QuidditchTeamsBrowserRouter = Backbone.Router.extend({
   routes: {
-    "" : "defaultRoute",
-    "teams" : "getTeamsGallery",
-    "teams/:id" : "getTeamProfile"
+    "(/)" : "defaultRoute",
+    "teams(/)" : "getTeamsGallery",
+    "teams/:id(/)" : "getTeamProfile",
+    ":whatever": "notFound"
   },
   navigateToHome: function() {
-    app.router.navigate('/');
+    app.router.navigate('/', true);
   },
   defaultRoute: function() {
     app.mainView.showPage('home');
   },
   getTeamsGallery: function() {
-    console.log('gallery');
+    this.navigateToHome();
   },
   getTeamProfile: function(id) {
+    if (isNaN(id)) {
+      this.navigateToHome();
+      return;
+    }
+
     app.teamProfile = new app.TeamProfileView(id);
     app.mainView.showPage('team_profile');
+  },
+
+  notFound: function() {
+    this.navigateToHome();
   }
 });
